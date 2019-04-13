36fc6855522b7ef79a67ee55932f8275a38be1c6
joanpaon <joanpaon@gmail.com>
2019-04-13 15:55:51 +0200

    :sparkles: Código optimizado

--- src/org/japo/java/forms/GUI.java
+++ src/org/japo/java/forms/GUI.java
@@ -34,26 +34,49 @@ import org.japo.java.libraries.UtilesSwing;
  *
  * @author José A. Pacheco Ondoño - joanpaon@gmail.com
  */
-public class GUI extends JFrame {
+public final class GUI extends JFrame {
 
     // Propiedades App
-    public static final String PRP_LOOK_AND_FEEL = "look_and_feel";
-    public static final String PRP_FAVICON = "favicon";
+    public static final String PRP_FAVICON_RESOURCE = "favicon_resource";
+    public static final String PRP_FONT_BANNER_RESOURCE = "font_banner_resource";
+    public static final String PRP_FONT_INTERFACE_RESOURCE = "font_interface_resource";
+    public static final String PRP_FORM_HEIGHT = "form_height";
+    public static final String PRP_FORM_TITLE = "form_title";
+    public static final String PRP_FORM_WIDTH = "form_width";
+    public static final String PRP_LOOK_AND_FEEL_PROFILE = "look_and_feel_profile";
 
     // Valores por Defecto
-    public static final String DEF_LOOK_AND_FEEL = UtilesSwing.LNF_NIMBUS;
-    public static final String DEF_FAVICON = "img/favicon.png";
+    public static final String DEF_FAVICON_RESOURCE = "img/favicon.png";
+    public static final String DEF_FONT_BANNER_SYSTEM_NAME = UtilesSwing.FONT_LUCIDA_BRIGHT_NAME;
+    public static final String DEF_FONT_BANNER_FALLBACK_NAME = "Serif";
+    public static final String DEF_FONT_INTERFACE_SYSTEM_NAME = UtilesSwing.FONT_LUCIDA_SANS_NAME;
+    public static final String DEF_FONT_INTERFACE_FALLBACK_NAME = "Dialog";
+    public static final int DEF_FORM_HEIGHT = 300;
+    public static final String DEF_FORM_TITLE = "Swing Manual #10";
+    public static final int DEF_FORM_WIDTH = 500;
+    public static final String DEF_LOOK_AND_FEEL_PROFILE = UtilesSwing.LNF_WINDOWS_PROFILE;
 
     // Referencias
     private Properties prp;
+    
+    // Componentes
     private JCheckBox cbxNegrita;
     private JCheckBox cbxCursiva;
     private JLabel lblRotulo;
+    private JPanel pnlControl;
+    private JPanel pnlPpal;
+    
+    // Fuentes
+    private Font fntRotulo;
+    private Font fntInterfaz;
 
     // Constructor
     public GUI(Properties prp) {
+        // Conectar Referencia
+        this.prp = prp;
+
         // Inicialización Anterior
-        initBefore(prp);
+        initBefore();
 
         // Creación Interfaz
         initComponents();
@@ -64,69 +87,84 @@ public class GUI extends JFrame {
 
     // Construcción del IGU
     private void initComponents() {
+        // Fuentes
+        fntRotulo = UtilesSwing.generarFuenteRecurso(
+                prp.getProperty(PRP_FONT_BANNER_RESOURCE),
+                DEF_FONT_BANNER_SYSTEM_NAME,
+                DEF_FONT_BANNER_FALLBACK_NAME);
+        fntInterfaz = UtilesSwing.generarFuenteRecurso(
+                prp.getProperty(PRP_FONT_INTERFACE_RESOURCE), 
+                DEF_FONT_INTERFACE_SYSTEM_NAME,
+                DEF_FONT_INTERFACE_FALLBACK_NAME);
+                
         // Etiqueta de prueba
         lblRotulo = new JLabel();
-        lblRotulo.setFont(new Font("Georgia", Font.PLAIN, 40));
-        lblRotulo.setText("Érase una vez Java");
-        lblRotulo.setHorizontalAlignment(JLabel.CENTER);
-        lblRotulo.setBorder(new BevelBorder(BevelBorder.LOWERED));
-        lblRotulo.setOpaque(true);
         lblRotulo.setBackground(Color.WHITE);
+        lblRotulo.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        lblRotulo.setFont(fntRotulo.deriveFont(Font.PLAIN, 40));
+        lblRotulo.setHorizontalAlignment(JLabel.CENTER);
+        lblRotulo.setOpaque(true);
+        lblRotulo.setText("Érase una vez Java");
 
         // Selector de negrita
         cbxNegrita = new JCheckBox("Negrita");
         cbxNegrita.addActionListener(new AEM(this));
-        cbxNegrita.setPreferredSize(new Dimension(200, 35));
-        cbxNegrita.setOpaque(false);
-        cbxNegrita.setFont(new Font("Cambria", Font.PLAIN, 30));
+        cbxNegrita.setFont(fntInterfaz.deriveFont(Font.PLAIN, 20));
         cbxNegrita.setHorizontalAlignment(JCheckBox.CENTER);
+        cbxNegrita.setOpaque(false);
+        cbxNegrita.setPreferredSize(new Dimension(200, 35));
 
         // Selector de cursiva
         cbxCursiva = new JCheckBox("Cursiva");
         cbxCursiva.addActionListener(new AEM(this));
-        cbxCursiva.setPreferredSize(new Dimension(200, 35));
-        cbxCursiva.setOpaque(false);
-        cbxCursiva.setFont(new Font("Cambria", Font.PLAIN, 30));
+        cbxCursiva.setFont(fntInterfaz.deriveFont(Font.PLAIN, 20));
         cbxCursiva.setHorizontalAlignment(JCheckBox.CENTER);
+        cbxCursiva.setOpaque(false);
+        cbxCursiva.setPreferredSize(new Dimension(200, 35));
 
         // Panel de control
-        JPanel pnlControl = new JPanel(new GridLayout(2, 1));
-        pnlControl.setBorder(new EmptyBorder(10, 10, 0, 10));
-        pnlControl.add(cbxNegrita);
+        pnlControl = new JPanel(new GridLayout(2, 1));
         pnlControl.add(cbxCursiva);
+        pnlControl.add(cbxNegrita);
+        pnlControl.setBorder(new EmptyBorder(10, 10, 10, 10));
 
         // Panel principal
-        JPanel pnlPpal = new JPanel(new GridLayout(2, 1));
-        pnlPpal.setBorder(new EmptyBorder(10, 10, 10, 10));
+        pnlPpal = new JPanel(new GridLayout(2, 1));
         pnlPpal.add(lblRotulo);
         pnlPpal.add(pnlControl);
+        pnlPpal.setBorder(new EmptyBorder(10, 10, 10, 10));
 
         // Ventana principal
         setContentPane(pnlPpal);
-        setTitle("Swing Manual #10");
+        setTitle(prp.getProperty(PRP_FORM_TITLE, DEF_FORM_TITLE));
         setResizable(false);
-        setSize(500, 300);
+        try {
+            int height = Integer.parseInt(prp.getProperty(PRP_FORM_HEIGHT));
+            int width = Integer.parseInt(prp.getProperty(PRP_FORM_WIDTH));
+            setSize(width, height);
+        } catch (NumberFormatException e) {
+            setSize(DEF_FORM_WIDTH, DEF_FORM_HEIGHT);
+        }
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     // Inicialización Anterior    
-    private void initBefore(Properties prp) {
-        // Memorizar Referencia
-        this.prp = prp;
-
+    private void initBefore() {
         // Establecer LnF
-        UtilesSwing.establecerLnF(prp.getProperty(PRP_LOOK_AND_FEEL, DEF_LOOK_AND_FEEL));
+        UtilesSwing.establecerLnFProfile(prp.getProperty(
+                PRP_LOOK_AND_FEEL_PROFILE, DEF_LOOK_AND_FEEL_PROFILE));
     }
 
     // Inicialización Posterior
     private void initAfter() {
         // Establecer Favicon
-        UtilesSwing.establecerFavicon(this, prp.getProperty(PRP_FAVICON, DEF_FAVICON));
+        UtilesSwing.establecerFavicon(this, prp.getProperty(
+                PRP_FAVICON_RESOURCE, DEF_FAVICON_RESOURCE));
     }
 
     // Procesar Estilo
-    public void procesarEstilo(ActionEvent e) {
+    public final void procesarEstilo(ActionEvent e) {
         // Valores Actuales Fuente
         String familia = lblRotulo.getFont().getFamily();
         int estilo = lblRotulo.getFont().getStyle();
